45d8c3c2a5701947843a0f8b11ed71698b9bf330
Shresth Kaushik <kaushikshresth22@gmail.com>
2024-07-21 14:32:07 +0530

    commit

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -156,7 +156,7 @@ exports.login = async (req, res) => {
       const options = {
         expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
         httpOnly: true,
-      }
+      } 
       res.cookie("token", token, options).status(200).json({
         success: true,
         token,
